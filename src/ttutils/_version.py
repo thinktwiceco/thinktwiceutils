bcc58c348b7aabf89908df7b3cb0f719a10e465b
GitHub Action <action@github.com>
2025-10-25 23:12:20 +0000

    Auto-format code [skip ci]

--- src/ttutils/_version.py
+++ src/ttutils/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev15+gc63e67692.d20251025'
-__version_tuple__ = version_tuple = (0, 1, 'dev15', 'gc63e67692.d20251025')
+__version__ = version = '0.0.3.dev12+g6efd7944b.d20251025'
+__version_tuple__ = version_tuple = (0, 0, 3, 'dev12', 'g6efd7944b.d20251025')
 
-__commit_id__ = commit_id = 'gc63e67692'
+__commit_id__ = commit_id = 'g6efd7944b'
